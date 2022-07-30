a1d500a4591ae149353905457b6496dd4ff8ecef
marcos.lima.eulabs <marcos.lima@eulabs.com.br>
2022-07-30 15:12:55 -0300

    Update criarbanco.sql
    Melhoria e sequencial das tabelas cliente e produto

--- src/database/criarbanco.sql
+++ src/database/criarbanco.sql
@@ -1,21 +1,20 @@
-DO $$ BEGIN
+-- DROPS
+	
+DROP TABLE IF EXISTS item_do_pedido;
+DROP TABLE IF EXISTS pedido;
+DROP TABLE IF EXISTS produto;
+DROP TABLE IF EXISTS cliente;  
+DROP SEQUENCE IF EXISTS seq_cliente_id;
+DROP SEQUENCE IF EXISTS seq_pedido_id;
+DROP SEQUENCE IF EXISTS seq_produto_id;     
 
-	IF EXISTS(select 1 from information_schema.tables where table_name='item_do_pedido') THEN
-     drop table item_do_pedido;
-  END IF;
+-- SEQUENCIAS 
 
-  IF EXISTS(select 1 from information_schema.tables where table_name='pedido') THEN
-     drop table pedido;
-  END IF;
+CREATE SEQUENCE seq_cliente_id INCREMENT 1 MINVALUE 1 MAXVALUE 9223372036854775807 START 1 CACHE 1;
+CREATE SEQUENCE seq_pedido_id INCREMENT 1 MINVALUE 1 MAXVALUE 9223372036854775807 START 1 CACHE 1;
+CREATE SEQUENCE seq_produto_id INCREMENT 1 MINVALUE 1 MAXVALUE 9223372036854775807 START 1 CACHE 1;
 
-  IF EXISTS(select 1 from information_schema.tables where table_name='produto') THEN
-     drop table produto;
-  END IF;
-
-  IF EXISTS(select 1 from information_schema.tables where table_name='cliente') THEN
-     drop table cliente;
-  END IF;
-END $$;
+-- TABELAS
 
 create table produto(
 	id int not null primary key,
@@ -24,6 +23,7 @@ create table produto(
 
 create table cliente(
 	id int not null primary key,
+	cpf varchar(11) not null,
 	nome varchar(50) not null,
 	sobrenome varchar(50) not null
 );
@@ -45,76 +45,76 @@ create table item_do_pedido(
 
 -- MASSA DE DADOS
 
-insert into cliente (id, nome, sobrenome) values
-(1, 'Marcos', 'Lima'),
-(2, 'Ana', 'Massaneiro'),
-(3, 'Giovana', 'Barreto'),
-(4, 'Robson', 'Rodrigues'), 
-(5, 'Pablya', 'Marchette');
+insert into cliente (id, cpf, nome, sobrenome) values
+(nextval('seq_cliente_id'), '98901179083', 'Marcos', 'Lima'),
+(nextval('seq_cliente_id'), '90397288018', 'Ana', 'Massaneiro'),
+(nextval('seq_cliente_id'), '80948890045', 'Giovana', 'Barreto'),
+(nextval('seq_cliente_id'), '54053704014', 'Robson', 'Rodrigues'), 
+(nextval('seq_cliente_id'), '21666147010', 'Pablya', 'Marchette');
 
 insert into produto (id, descricao) values
-(1, 'Açúcar'),
-(2, 'Sal'),
-(3, 'Arroz'),
-(4, 'Feijão'),
-(5, 'Farinha'),
-(6, 'Macarrão'),
-(7, 'Café'),
-(8, 'Leite'),
-(9, 'Óleo'),
-(10, 'Temperos'),
-(11, 'Molho de tomate'),
-(12, 'Queijo ralado'),
-(13, 'Ovos'),
-(14, 'Fermento'),
-(15, 'Pães'),
-(16, 'Carnes'),
-(17, 'Iogurte'),
-(18, 'Margarina ou manteiga'),
-(19, 'Maisena'),
-(20, 'Biscoitos'),
-(21, 'Hortaliças em geral'),
-(22, 'Produtos de limpeza'),
-(23, 'Sabão em pedra'),
-(24, 'Sabão em pó'),
-(25, 'Detergente'),
-(26, 'Desinfetante'),
-(27, 'Amaciante'),
-(28, 'Lustra-móveis'),
-(29, 'Álcool em gel'),
-(30, 'Água sanitária'),
-(31, 'Inseticida'),
-(32, 'Esponja de pia'),
-(33, 'Esponja de aço'),
-(34, 'Sacos de lixo'),
-(35, 'Luvas plásticas'),
-(36, 'Flanelas'),
-(37, 'Produtos de higiene e uso pessoal'),
-(38, 'Sabonete'),
-(39, 'Creme dental'),
-(40, 'Escova de dente'),
-(41, 'Fio dental'),
-(42, 'Absorventes'),
-(43, 'Barbeador descartável'),
-(44, 'Creme de barbear'),
-(45, 'Algodão'),
-(46, 'Desodorante'),
-(47, 'Shampoo e condicionador'),
-(48, 'Papel higiênico'),
-(49, 'Água oxigenada'),
-(50, 'Gaze'),
-(51, 'Hastes flexíveis'),
-(52, 'Esparadrapo'),
-(53, 'Curativos'),
-(54, 'Produtos úteis para o dia a dia'),
-(55, 'Papel alumínio'),
-(56, 'Papel filme'),
-(57, 'Papel toalha'),
-(58, 'Guardanapo de papel'),
-(59, 'Fósforo'),
-(60, 'Velas'),
-(61, 'Lâmpadas'),
-(62, 'Fita isolante'),
-(63, 'Fita crepe');
+(nextval('seq_produto_id'), 'Açúcar'),
+(nextval('seq_produto_id'), 'Sal'),
+(nextval('seq_produto_id'), 'Arroz'),
+(nextval('seq_produto_id'), 'Feijão'),
+(nextval('seq_produto_id'), 'Farinha'),
+(nextval('seq_produto_id'), 'Macarrão'),
+(nextval('seq_produto_id'), 'Café'),
+(nextval('seq_produto_id'), 'Leite'),
+(nextval('seq_produto_id'), 'Óleo'),
+(nextval('seq_produto_id'), 'Temperos'),
+(nextval('seq_produto_id'), 'Molho de tomate'),
+(nextval('seq_produto_id'), 'Queijo ralado'),
+(nextval('seq_produto_id'), 'Ovos'),
+(nextval('seq_produto_id'), 'Fermento'),
+(nextval('seq_produto_id'), 'Pães'),
+(nextval('seq_produto_id'), 'Carnes'),
+(nextval('seq_produto_id'), 'Iogurte'),
+(nextval('seq_produto_id'), 'Margarina ou manteiga'),
+(nextval('seq_produto_id'), 'Maisena'),
+(nextval('seq_produto_id'), 'Biscoitos'),
+(nextval('seq_produto_id'), 'Hortaliças em geral'),
+(nextval('seq_produto_id'), 'Produtos de limpeza'),
+(nextval('seq_produto_id'), 'Sabão em pedra'),
+(nextval('seq_produto_id'), 'Sabão em pó'),
+(nextval('seq_produto_id'), 'Detergente'),
+(nextval('seq_produto_id'), 'Desinfetante'),
+(nextval('seq_produto_id'), 'Amaciante'),
+(nextval('seq_produto_id'), 'Lustra-móveis'),
+(nextval('seq_produto_id'), 'Álcool em gel'),
+(nextval('seq_produto_id'), 'Água sanitária'),
+(nextval('seq_produto_id'), 'Inseticida'),
+(nextval('seq_produto_id'), 'Esponja de pia'),
+(nextval('seq_produto_id'), 'Esponja de aço'),
+(nextval('seq_produto_id'), 'Sacos de lixo'),
+(nextval('seq_produto_id'), 'Luvas plásticas'),
+(nextval('seq_produto_id'), 'Flanelas'),
+(nextval('seq_produto_id'), 'Produtos de higiene e uso pessoal'),
+(nextval('seq_produto_id'), 'Sabonete'),
+(nextval('seq_produto_id'), 'Creme dental'),
+(nextval('seq_produto_id'), 'Escova de dente'),
+(nextval('seq_produto_id'), 'Fio dental'),
+(nextval('seq_produto_id'), 'Absorventes'),
+(nextval('seq_produto_id'), 'Barbeador descartável'),
+(nextval('seq_produto_id'), 'Creme de barbear'),
+(nextval('seq_produto_id'), 'Algodão'),
+(nextval('seq_produto_id'), 'Desodorante'),
+(nextval('seq_produto_id'), 'Shampoo e condicionador'),
+(nextval('seq_produto_id'), 'Papel higiênico'),
+(nextval('seq_produto_id'), 'Água oxigenada'),
+(nextval('seq_produto_id'), 'Gaze'),
+(nextval('seq_produto_id'), 'Hastes flexíveis'),
+(nextval('seq_produto_id'), 'Esparadrapo'),
+(nextval('seq_produto_id'), 'Curativos'),
+(nextval('seq_produto_id'), 'Produtos úteis para o dia a dia'),
+(nextval('seq_produto_id'), 'Papel alumínio'),
+(nextval('seq_produto_id'), 'Papel filme'),
+(nextval('seq_produto_id'), 'Papel toalha'),
+(nextval('seq_produto_id'), 'Guardanapo de papel'),
+(nextval('seq_produto_id'), 'Fósforo'),
+(nextval('seq_produto_id'), 'Velas'),
+(nextval('seq_produto_id'), 'Lâmpadas'),
+(nextval('seq_produto_id'), 'Fita isolante'),
+(nextval('seq_produto_id'), 'Fita crepe');
 
 select 'CRIADO COM SUCESSO!'
